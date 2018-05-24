f3b6cae86f61a8c6bfd5d4495c342632fd2a0006
Zachary Chestler <chestlez@flip3.engr.oregonstate.edu>
2018-05-24 15:20:35 -0700

    forever npm added

--- Project/app.js
+++ Project/app.js
@@ -12,7 +12,7 @@ app.use(express.static('public'))
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
-app.set('port', 3000);
+app.set('port', 5377);
 
 app.get('/',function(req,res){
   res.render('home')
